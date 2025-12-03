c563177518951e6eebb70d9b3c6f187e98749183
Junaidarmaan <120014051+Junaidarmaan@users.noreply.github.com>
2025-12-03 08:30:41 +0000

    added questions repository

--- backend/app/src/main/java/com/haisy/app/Repository/QuestionsRepository.java
+++ backend/app/src/main/java/com/haisy/app/Repository/QuestionsRepository.java
@@ -1,9 +1,11 @@
 package com.haisy.app.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.haisy.app.Model.Question;
 
+@Repository
 public interface QuestionsRepository extends JpaRepository<Question,Integer> {
     Question findByIdAndQuizId(int id, int quizId);
 }
