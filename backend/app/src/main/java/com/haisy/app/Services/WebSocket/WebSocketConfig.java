26373ecafd252cc216e40347075e5238f1f6c41c
Junaidarmaan <120014051+Junaidarmaan@users.noreply.github.com>
2025-12-06 10:26:52 +0000

    fixed few typos 3

--- backend/app/src/main/java/com/haisy/app/Services/WebSocket/WebSocketConfig.java
+++ backend/app/src/main/java/com/haisy/app/Services/WebSocket/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/server").setAllowedOrigins("https://smart-quiz-xmzm.onrender.com").withSockJS();
+        registry.addEndpoint("/server").setAllowedOrigins("https://ominous-disco-w6grj7qxw6xcjpx-3000.app.github.dev").withSockJS();
     }
 
     @Override
